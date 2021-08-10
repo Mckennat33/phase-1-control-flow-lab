1cf153c84f5af101a924dad06e209c5d33493f43
Thomas <thomasmckenna12gmail.com>
2021-08-10 12:08:35 -0400

    phase-1-control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){ 
+   
+    if (number <= 400) {
+      return 'This one is on me!'
+    } else if (number >= 2001 && number < 2501) {
+      return 'I will gladly take your thirty bucks.'
+    } else if (number >= 2501) {
+      return 'No can do.'
+    }
+  }
+    
+function ternaryCheckCity(city){
+
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      
+    case 'not as generous':
+      return 'Thank you.'; 
+
+    case 'thanks for everything':
+      return 'Bye.'
+}
 }
