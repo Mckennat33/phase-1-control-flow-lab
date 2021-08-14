952db9279c791a82989e1bb8ea90ee43faa00773
Thomas <thomasmckenna12gmail.com>
2021-08-14 15:55:36 -0400

    commit tree cee53a62dd28f0ec068b92ed1238df0241043422

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
+
+
+
+
 function scuberGreetingForFeet(number){ 
-   
-    if (number <= 400) {
-      return 'This one is on me!'
+
+      if (number <= 400) {
+        return 'This one is on me!'
     } else if (number >= 2001 && number < 2501) {
-      return 'I will gladly take your thirty bucks.'
+        return 'I will gladly take your thirty bucks.'
     } else if (number >= 2501) {
-      return 'No can do.'
+        return 'No can do.'
     }
   }
-    
+
 function ternaryCheckCity(city){
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
 
-  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
-  
 }
-
-
 
 function switchOnCharmFromTip(tip){
-  switch(tip) {
-    case 'generous':
+    switch(tip) {
+      case 'generous':
       return 'Thank you so much.';
-      
+
     case 'not as generous':
       return 'Thank you.'; 
+      
+    default: 'thanks for everything';
+     return 'Bye.';
+}
+}
 
-    case 'thanks for everything':
-      return 'Bye.'
-}
-}
